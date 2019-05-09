946891808dbf6b72f448f9beecf8398766aa170f
DiegoLuque <dichavez@socacesores.com>
2019-05-09 12:56:26 -0500

    Mi primer comit

---
+++ WebApplication2/Controllers/StudentController.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Web.Mvc
+
+
+
+
+
+Namespace Controllers
+
+    Public Class StudentController
+        Inherits Controller
+
+        ' GET: Student
+        Function Index() As ActionResult
+            var studentList = New List < Student > {
+                            New Student() {StudentId = 1, StudentName = "John", Age = 18},
+                            New Student() {StudentId = 2, StudentName = "Steve", Age = 21},
+                            New Student() {StudentId = 3, StudentName = "Bill", Age = 25},
+                            New Student() {StudentId = 4, StudentName = "Ram", Age = 20},
+                            New Student() {StudentId = 5, StudentName = "Ron", Age = 31},
+                            New Student() {StudentId = 4, StudentName = "Chris", Age = 17},
+                            New Student() {StudentId = 4, StudentName = "Rob", Age = 19}
+                        };
+        // Get the students from the database in the real application
+
+        Return View(studentList);
+        End Function
+    End Class
+End Namespace
---
+++ WebApplication2/Models/Student.vb
@@ -0,0 +1,9 @@
+﻿Namespace WebApplication2.Models
+    Public Class Student
+        Public int StudentId { Get; Set; }
+        Public String StudentName { Get; Set; }
+        Public int Age { Get; Set; }
+    End Class
+End Namespace
+
+
